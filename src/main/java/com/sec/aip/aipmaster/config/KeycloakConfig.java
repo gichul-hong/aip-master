a77d458a9a96d3925eacf258cf065ffe1b16be7d
hong-gichul <hong-gichul@localhost>
2021-07-22 16:49:19 +0900

    keycloak 연동을 위한 keycloakAdminService 추가

--- src/main/java/com/sec/aip/aipmaster/config/KeycloakConfig.java
+++ src/main/java/com/sec/aip/aipmaster/config/KeycloakConfig.java
@@ -4,30 +4,43 @@ import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.admin.client.resource.RealmResource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class KeycloakConfig {
 
+	@Value("${keycloak.realmname}")
+	String aipRealmName;
+	
+	@Value("${keycloak.clientname}")
+	String aipClientId;
+    
 	@Bean
-	public Keycloak keycloakClient(String realmName) {
+	public Keycloak aipKeycloakClient() {
 		
+		Keycloak keycloakClient = KeycloakBuilder.builder()
+				.serverUrl("http://localhost:8080/auth")
+				.grantType(OAuth2Constants.PASSWORD)
+				.realm(aipRealmName)
+				.clientId(aipClientId)
+//				.clientSecret("bf56a959-5d5e-457d-a781-8ffcd6cd159e")
+//				.username("gichul.hong")
+				.username("aip-admin")
+				.password("1234")
+				.resteasyClient(
+						new ResteasyClientBuilder()
+						.connectionPoolSize(10).build()
+						).build();
+		
+		return keycloakClient;
 	}
-	Keycloak keycloak = KeycloakBuilder.builder()
-		    .serverUrl("http://localhost:8080/auth")
-		    .grantType(OAuth2Constants.PASSWORD)
-		    .realm("realm-name")
-		    .clientId("keycloak-admin")
-		    .username("username")
-		    .password("password")
-		    .resteasyClient(
-		        new ResteasyClientBuilder()
-		            .connectionPoolSize(10).build()
-		    ).build();
-
-//		keycloak.tokenManager().getAccessToken();
-		RealmResource realmResource = keycloak.realm("realm-name");
+	
+//	@Bean
+//	public RealmResource aipRealm(Keycloak aipKeycloakClient) {
+//		
+//		return aipKeycloakClient.realm("aip");
+//	}
 	
 }
